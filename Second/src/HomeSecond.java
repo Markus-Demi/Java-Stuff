780940373f28295eb7fd3f0d35d037746d963ca9
Markus-Demi <L7ipo1h11da6r6e6!>
2017-11-19 22:37:27 +0300

    Part of homework 2, tasks 2 and 3

--- Second/src/HomeSecond.java
+++ Second/src/HomeSecond.java
@@ -1,5 +1,6 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class HomeSecond {
@@ -23,12 +24,62 @@ public class HomeSecond {
                 arr[i] = ninetyN;
             }
         }
-        String intArrStr = java.util.Arrays.toString(arr);
+        String intArrStr = Arrays.toString(arr);
         System.out.println("min = " + min + "\nmax = " + max);
         System.out.println(intArrStr);
 
         System.out.println("\n////////////////////////////////////");
 
+        // 2. Array of floats with repeatable nums
+
+        int[] arrF = new int[size];
+
+        for(int i = 0; i < size; i++) {
+            arrF[i] = rand.nextInt() % 10;
+        }
+
+        for(int i = 0; i < size; i++) {
+
+            //int repeatNums = 0;
+
+            for(int j = i + 1; j < size; j++) {
+                if(arrF[i] == arrF[j] && i != j) {
+                    System.out.println("Repeated nums at element " + i + " and " + j + " in array");
+                    //repeatNums++;
+                }
+            }
+        /*    if(repeatNums > 1) {
+                System.out.println("[" + arrF[i] + "]" + " - repeats " + repeatNums);
+            } */
+        }
+        System.out.println(Arrays.toString(arrF));
+
+        System.out.println("\n////////////////////////////////////");
+
+        // 3. Revert an array
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt() % 10;
+        }
+        System.out.println("Array: " + Arrays.toString(array));
+
+        for (int i = 0, j = array.length - 1; i < array.length / 2 && j / 2 >= 0; i++, j--) {
+            int tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+
+        System.out.println("Revert array: " + Arrays.toString(array));
+
+        /*
+
+4. Написать программу, определяющую, образуют ли цифры некоторого числа строго возрастающую последовательность.
+Например: 123 – образуют, 212 – не образуют.
+
+5.Создайте массив типа int. Отсортируйте массив любым способом (по убыванию либо по возрастанию). Р
+езультат вывести на экран.
+
+         */
 
     }
 }
