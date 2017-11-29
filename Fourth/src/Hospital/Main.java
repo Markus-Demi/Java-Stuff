1003603f9dbfc7af6376b10d3fa3178759e1b05f
Markus-Demi <L7ipo1h11da6r6e6!>
2017-11-29 20:52:37 +0300

    Major fixes to Patient Homework. It's done.

--- Fourth/src/Hospital/Main.java
+++ Fourth/src/Hospital/Main.java
@@ -1,42 +1,9 @@
 package Hospital;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         HospitalManager manager = new HospitalManager();
-
         manager.createPatients();
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a num to swtich between:\n1.Show all patients." +
-                "\n2.Search a patient by his name.\n3.Search a patient by his age");
-        int switchCommand = sc.nextInt();
-        switch(switchCommand) {
-            case 1: {
-                Patient[] pArr = manager.getPatients();
-                if(pArr != null) {
-                    for (int i = 0; i < pArr.length; i++) {
-                        System.out.println("Patient - " + pArr[i].getName() + ", age - " + pArr[i].getAge());
-                    }
-                } else {
-                    System.out.println("In the hospital no one patient.");
-                }
-                break;
-            }
-            case 2: {
-
-                break;
-            }
-            case 3: {
-
-                break;
-            }
-            default: {
-
-                break;
-            }
-        }
-
+        manager.hospitalMenu();
     }
 }
