d852a8a43ca8d061e665c1c8ce3e9f0cbc623f50
Markus-Demi <L7ipo1h11da6r6e6!>
2017-11-29 21:40:29 +0300

    Fixed homework 3

---
+++ HomeWork3/src/com/gmail/m4rk/task2/Main.java
@@ -0,0 +1,27 @@
+package com.gmail.m4rk.task2;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int news = rnd.nextInt() % 100;
+
+        System.out.println("Number of news: " + news);
+
+        System.out.println("Number of pages: " + paperCounter(news));
+    }
+
+    private static int paperCounter(int newsOnPage) {
+        int pCounter = 0;
+
+        if(newsOnPage > 0) {
+            pCounter = (newsOnPage / 10);
+            if(((newsOnPage / 10) % 10) > 0 && ((newsOnPage / 10) % 10) < 10 || (newsOnPage / 10 % 10) < 10) {
+                pCounter += 1;
+            }
+        }
+
+        return pCounter;
+    }
+}
